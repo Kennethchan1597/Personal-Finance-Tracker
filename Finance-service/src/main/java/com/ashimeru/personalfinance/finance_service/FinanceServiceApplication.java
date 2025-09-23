c9480b63190c1d25946153fa23d6c4f59d3ae1a7
ashimeru <hkchan229@gmail.com>
2025-09-23 21:35:27 +0800

    finance-service-created

---
+++ Finance-service/src/main/java/com/ashimeru/personalfinance/finance_service/FinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ashimeru.personalfinance.finance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceServiceApplication.class, args);
+	}
+
+}
